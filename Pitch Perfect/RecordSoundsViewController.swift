0a502a9231c12abcf9798d9c786b7852ca1a9082
Billy <mr.bill.ly@gmail.com>
2016-11-26 11:40:42 -0800

    pitch perfect project files

---
+++ Pitch Perfect/RecordSoundsViewController.swift
@@ -0,0 +1,62 @@
+//
+//  RecordSoundsViewController.swift
+//  Pitch Perfect
+//
+//  Created by Billy on 11/15/16.
+//  Copyright © 2016 Billy. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+
+class RecordSoundsViewController: UIViewController {
+
+    var audioRecorder: AVAudioRecorder!
+    
+    @IBOutlet weak var recordingLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        stopRecordingButton.isEnabled = false
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
+
+
+    @IBAction func recordAudio(_ sender: AnyObject) {
+        recordingLabel.text = "Recording in Progress"
+        stopRecordingButton.isEnabled = true
+        recordButton.isEnabled = false
+        
+        let dirPath = NSSearchPathForDirectoriesInDomains(.documentDirectory,.userDomainMask, true)[0] as String
+        let recordingName = "recordedVoice.wav"
+        let pathArray = [dirPath, recordingName]
+        let filePath = URL(string: pathArray.joined(separator: "/"))
+        
+        let session = AVAudioSession.sharedInstance()
+        try! session.setCategory(AVAudioSessionCategoryPlayAndRecord, with: AVAudioSessionCategoryOptions.defaultToSpeaker);
+        
+        try! audioRecorder = AVAudioRecorder(url: filePath!, settings: [:])
+//        audioRecorder.delegate = self
+        audioRecorder.isMeteringEnabled = true
+        audioRecorder.prepareToRecord()
+        audioRecorder.record()
+        
+    }
+    
+    @IBOutlet weak var recordButton: UIButton!
+
+    @IBAction func stopRecording(_ sender: AnyObject) {
+        recordButton.isEnabled = true
+        stopRecordingButton.isEnabled = false
+        recordingLabel.text = "Tap to Record"
+        
+    }
+    
+    @IBOutlet weak var stopRecordingButton: UIButton!
+}
+
